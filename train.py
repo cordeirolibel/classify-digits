ca7cac9232bc8a5f92fb9a55a3d472beb319ceb2
cordeirolibel <gustavolibel@gmail.com>
2017-10-07 23:15:23 -0300

    fix fig

--- train.py
+++ train.py
@@ -8,32 +8,28 @@ import mnist_loader as ml
 from pca import Pca
 
 #training_data:50000  validation_data:10000  test_data:10000 images
-training_data, validation_data, test_data = ml.load_data_wrapper('data/mnist.pkl.gz')
+training_data, validation_data, test_data = ml.load_data_wrapper('data/mnist.pkl.gz')#,only_num = 3)
 
 #net = Network([784, 20, 10])
-
-net = Network([2,20, 10])
+net = Network([10,20, 10])
 net.cost_function = 'cross-entropy'
 
 tic()
 
-pca = Pca(2)
+pca = Pca(10)
 training_data,test_data = pca.run(training_data,test_data)
-
-#pca.images_save()
-
+pca.images_save()
 print(tic(),'ms')
 
 net.SGD(training_data, 5, 10, 0.5, test_data=test_data)
 #net.weights_img_save()
 
-tic()
-predict_data = net.predict(test_data)
 
-pca.plot(test_data,pause=False,name='Test Dataset')
-pca.plot(predict_data,name='Predict Dataset')
+#predict_data = net.predict(test_data)
+#pca.plot(test_data,pause=False,name='Test Dataset')
+#pca.plot(predict_data,name='Predict Dataset')
 
-print(tic(),'ms')
+
 
 print('end')
 
